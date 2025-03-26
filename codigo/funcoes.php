58b927e3984d0a7b7fc58aaf4ef358ada9d39cbc
bianckkj <biancaavitoria1236@gmail.com>
2025-03-26 08:23:24 -0300

    novos arquivos

---
+++ codigo/funcoes.php
@@ -0,0 +1,82 @@
+<?php
+
+function deletarCliente($conexao, $idcliente) {
+    $sql = "DELETE FROM tb_cliente WHERE idcliente = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_bind_param($comando, 'i', $idcliente);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    
+    return $funcionou; //true ou false
+}
+
+function listarClientes($conexao) {
+    $sql = "SELECT * FROM tb_cliente";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $lista_clientes = [];
+    while ($cliente = mysqli_fetch_assoc($resultado)) {
+        $lista_clientes[] = $cliente;
+    }
+
+    mysqli_stmt_close($comando);
+    return $lista_clientes;
+}
+
+function salvarCliente($conexao, $nome, $cpf, $endereco) {
+    $sql = "INSERT INTO tb_cliente (nome, cpf, endereco) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sss', $nome, $cpf, $endereco);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
+
+function deletarProduto($conexao, $idproduto) {
+    $sql = "DELETE FROM tb_produto WHERE idproduto = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_bind_param($comando, 'i', $idproduto);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    
+    return $funcionou; //true ou false
+}
+
+function listarProdutos($conexao) {
+    $sql = "SELECT * FROM tb_produto";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $lista_produtos = [];
+    while ($produto = mysqli_fetch_assoc($resultado)) {
+        $lista_produtos[] = $produto;
+    }
+
+    mysqli_stmt_close($comando);
+    return $lista_produtos;
+};
+
+function salvarProdutos($conexao, $nome, $tipo, $preco_compra, $preco_venda, $margem_lucro, $quantidade) {
+    $sql = "INSERT INTO tb_produto (nome, tipo, preco_compra, preco_venda, margem_lucro, quantidade) VALUES (?, ?, ?, ?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'ssdddd', $nome, $tipo, $preco_compra, $preco_venda, $margem_lucro, $quantidade);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
+?>
---
+++ codigo/tests/teste_salvarProduto.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "../conexao.php";
+require_once "../funcoes.php";
+
+
+salvarProdutos($conexao, $nome, $tipo, $preco_compra, $preco_venda, $margem_lucro, $quantidade);
