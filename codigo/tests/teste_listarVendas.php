e2b7084a81337024895a30d6f527957732d8c64b
bianckkj <biancaavitoria1236@gmail.com>
2025-04-04 21:40:10 -0300

    atualizando função list vendas

--- codigo/tests/teste_listarVendas.php
+++ codigo/tests/teste_listarVendas.php
@@ -1,9 +1,13 @@
 <?php
 require_once "../conexao.php";
 require_once "../funcoes.php";
-
 echo "<pre>";
-print_r(listarVendas($conexao));
+$vendas = listarVendas($conexao);
+foreach ($vendas as $venda) {
+    echo "id venda: {$venda['idvenda']}<br>";
+    echo "cliente: {$venda['nome_cliente']}<br>";
+    echo "produto: {$venda['nome_produto']}<br>";
+    echo "data da venda: {$venda['data']}<br>";
+}
 echo "</pre>";
-
 ?>
